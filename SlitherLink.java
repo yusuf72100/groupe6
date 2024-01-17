40a57e525538320ecf6d3c92591e3c4d3bd1eceb
Sultan <38288876+yusuf72100@users.noreply.github.com>
2024-01-17 14:43:34 +0100

    Serialisation / Deserialisation v1.0

--- SlitherLink.java
+++ SlitherLink.java
@@ -1,57 +1,68 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.*;
 
 /**
  * Cette classe sera la classe principale de notre programme
  */
 public class SlitherLink extends JFrame {
-    private static final long serialVersionUID = 1L;
-    
+    private static Grille grille;
     public SlitherLink(){
         super("Slitherlink");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        grille = new Grille();
+
+        grille.width = 5;
+        grille.height = 5;
+
+        // Définir la grille
+        grille.grid = Arrays.asList(
+            Arrays.asList(1, 0, 0, 0, 0),
+            Arrays.asList(0, 1, 0, 0, 0),
+            Arrays.asList(0, 0, 1, 0, 0),
+            Arrays.asList(0, 0, 0, 1, 0),
+            Arrays.asList(0, 0, 0, 0, 1)
+        );
+
+        // Définir le niveau
+        Map<String, Object> level = new HashMap<>();
+        level.put("width", grille.width);
+        level.put("height", grille.height);
+        level.put("grid", grille.grid);
+
+        grille.level = Arrays.asList(level);
     }
 
-    public static void main(String[] var){
+    public static void loadDatas() throws IOException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream("save.yml");
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            Grille myObject = (Grille) ois.readObject();
+            System.out.println("Objet chargé avec succès: " + myObject);
+        }
+    }
+
+    public static void saveDatas() throws IOException {
+        try (
+            FileOutputStream fos = new FileOutputStream("save.yml");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+            oos.writeObject(grille);
+            System.out.println("Objet sauvegardé avec succès.");
+
+        }
+    }
+
+    public static void main(String[] var) throws IOException, ClassNotFoundException {
         SlitherLink slitherLink = new SlitherLink();
         slitherLink.setVisible(true);
 
-        System.out.print("Ceci est un test jeune truite\n");
+        System.out.print("-----------------------Ceci est un test jeune truite-----------------------------\n");
 
-        ObjectOutputStream oos = null;
-
-        Grille grille = new Grille();
-        // Modification de la grille selon votre exemple YAML
-        // Vous devez implémenter les setters appropriés dans la classe Grille
-        // Par exemple : grille.setEasy(...);
-
-        // Sérialisation de l'objet Grille en format YAML
-        try (FileOutputStream fileOutputStream = new FileOutputStream("template_grille.yaml");
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
-
-            // Écrire l'objet Grille dans le fichier
-            objectOutputStream.writeObject(grille);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Désérialisation de l'objet Grille à partir du fichier
-        try (FileInputStream fileInputStream = new FileInputStream("template_grille.yaml");
-             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
-
-            // Lire l'objet Grille du fichier
-            Grille loadedGrille = (Grille) objectInputStream.readObject();
-
-            // Afficher la grille chargée
-            loadedGrille.displayGrid();
-
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        //saveDatas();
+        loadDatas();
+        System.out.print(grille.height);
     }
 }
