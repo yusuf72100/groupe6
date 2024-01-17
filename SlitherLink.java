ecdf9263fba769de70fd4370f52113bc436b411e
Yusuf Ulas <yusufulas481@gmail.com>
2024-01-17 11:04:02 +0100

    first test json

--- SlitherLink.java
+++ SlitherLink.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.swing.*;
 
@@ -11,13 +16,33 @@ public class SlitherLink extends JFrame {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
+    private static String lireContenuFichier(String cheminFichier) throws IOException {
+        Path chemin = Paths.get(cheminFichier);
+        
+        // Utiliser Files.readString pour lire le contenu du fichier en tant que chaîne
+        return Files.readString(chemin);
+    }
+
     public static void main(String[] var){
         SlitherLink slitherLink = new SlitherLink();
         slitherLink.setVisible(true);
         System.out.print("Ceci est un test jeune truite\n");
 
-        JSONObject jsonO = (JSONObject) jsonP.parse(new FileReader("./template_grille.json"));
+        String cheminFichier = "chemin/vers/votre/fichier.json";
 
-        
+        try {
+            // Lire le contenu du fichier dans une chaîne
+            String contenuFichier = lireContenuFichier("./template_grille.json");
+
+            // Manipuler le contenu JSON comme une chaîne
+            System.out.println(contenuFichier);
+
+            // Vous pouvez ensuite traiter manuellement la chaîne JSON selon vos besoins
+            // Par exemple, vous pouvez utiliser des opérations de la classe String pour extraire des données.
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
