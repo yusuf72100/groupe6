3d6067f2a1af6462b2ba9359991d56761d094752
Yusuf Ulas <38288876+yusuf72100@users.noreply.github.com>
2024-02-23 11:03:28 -1200

    Small update

--- src/main/java/org/groupe6/slitherlink/PuzzleGenerator/Main.java
+++ src/main/java/org/groupe6/slitherlink/PuzzleGenerator/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+//
     /**
      * On affiche une toute nouvelle grille
      * @param longueur
