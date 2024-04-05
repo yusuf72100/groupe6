a00380f4d78d8517f5ff027be86950b5684ca389
Sultan <38288876+yusuf72100@users.noreply.github.com>
2024-04-05 12:04:50 +0200

    SaveSelectionMenu first PH

--- src/main/java/groupe6/affichage/SaveSelectionMenu.java
+++ src/main/java/groupe6/affichage/SaveSelectionMenu.java
@@ -21,37 +21,74 @@ import javafx.util.Duration;
 import java.util.List;
 
 public class SaveSelectionMenu extends MainMenu {
-    private ComboBox<String> profilSelector;
+    private static Button backButton;
+    private static Label backText;
+    private static ComboBox<String> profilSelector;
+    private static String[] buttonTextsLabels;
+    private static HBox mainHbox;
+    private static StackPane mainPane;
+    private static HBox[] descriptionsBackground;
+    private static StackPane[] buttonsContainer;
+    private static Button[] buttons;
+    private static Label[] descriptionText;
+    private static Label[] buttonsText;
 
-    public static StackPane getMenu(Double windowWidth, Double windowHeigth) {
-        List<String> lstSave = CatalogueSauvegarde.listerSauvegarde(Launcher.catalogueProfils.getProfilActuel());
+    // animations
+    private static TranslateTransition[] rectangleTransition;
+    private static TranslateTransition[] rectangleTransitionReverse;
+    private static FadeTransition[] fadeTransition;
+    private static FadeTransition[] fadeTransitionReverse;
+    private static Rectangle[] clipRectangle;
+    private static Text title = new Text("Slitherlink");
+    private static List<Profil> profils;
+    private static List<String> lstSave;
+
+    public static void initMenu() {
+        lstSave = CatalogueSauvegarde.listerSauvegarde(Launcher.catalogueProfils.getProfilActuel());
         title = new Text("Choisissez une partie");
         backButton = new Button();
         mainHbox = new HBox();
         mainPane = new StackPane();
-        descriptionsBackground = new HBox[lstSave.size()];
-        buttonsContainer = new StackPane[lstSave.size()];
-        buttons = new Button[lstSave.size()];
-        descriptionText = new Label[lstSave.size()];
-        buttonsText = new Label[lstSave.size()];
+        buttonsContainer = new StackPane[lstSave.size()+10];
+        buttons = new Button[lstSave.size()+10];
+        descriptionText = new Label[lstSave.size()+10];
+        buttonsText = new Label[lstSave.size()+10];
 
         // animations
-        rectangleTransition = new TranslateTransition[3];
-        rectangleTransitionReverse = new TranslateTransition[3];
-        fadeTransition = new FadeTransition[3];
-        fadeTransitionReverse = new FadeTransition[3];
-        clipRectangle = new Rectangle[3];
+        fadeTransition = new FadeTransition[lstSave.size()+10];
+        fadeTransitionReverse = new FadeTransition[lstSave.size()+10];
         profils = Launcher.catalogueProfils.getListeProfils();
-        
-        backText.setText("RETOUR");
-        buttonTextsLabels = new String[lstSave.size()];
-        buttons = new Button[lstSave.size()];
 
-        for (int i = 0; i < buttons.length; i++) {
-            System.out.println(buttons.length);
-            buttonTextsLabels[i] = lstSave.get(i);
+        backText = new Label("RETOUR");
+        buttonTextsLabels = new String[lstSave.size()+10];
+        buttons = new Button[lstSave.size()+10];
+    }
+
+    private static String extraireDifficulte(String data) {
+        String difficulte = data.split("_")[0];
+        String premiereLettre = difficulte.substring(0, 1).toUpperCase();
+        String resteDuMot = difficulte.substring(1).toLowerCase();
+        return premiereLettre + resteDuMot;
+    }
+
+    private static String extraireTaille(String data) {
+        return data.split("_")[1];
+    }
+
+    private static String extraireDate(String data) {
+        return (data.split("_")[2] + "-" + data.split("_")[3] + "-" + data.split("_")[4]);
+    }
+
+    public static StackPane getMenu(Double windowWidth, Double windowHeigth) {
+        initMenu();
+        for (int i = 0; i < lstSave.size() + 10; i++) {
+            if(i >= lstSave.size()) buttonTextsLabels[i] = "Rien";
+            else buttonTextsLabels[i] = lstSave.get(i);
+
             buttons[i] = new Button();
-            buttons[i].setPrefSize(Menu.toPourcentWidth(350.0, windowWidth), Menu.toPourcentHeight(500.0, windowHeigth));
+            buttons[i].setMinSize(Menu.toPourcentWidth(350.0, windowWidth), Menu.toPourcentHeight(500.0, windowHeigth));
+            buttons[i].setMaxSize(Menu.toPourcentWidth(350.0, windowWidth), Menu.toPourcentHeight(500.0, windowHeigth));
+            //buttons[i].setPrefSize(Menu.toPourcentWidth(350.0, windowWidth), Menu.toPourcentHeight(500.0, windowHeigth));
             buttons[i].getStyleClass().add("button-rounded");
 
             buttonsText[i] = new Label(buttonTextsLabels[i]);
@@ -60,12 +97,6 @@ public class SaveSelectionMenu extends MainMenu {
             buttonsText[i].setMouseTransparent(true);
             buttonsText[i].setTextAlignment(TextAlignment.CENTER);
             Menu.adaptTextSize(buttonsText[i], 20, windowWidth, windowHeigth);
-
-            descriptionsBackground[i] = new HBox();
-            descriptionsBackground[i].setMaxSize(buttons[i].getPrefWidth(), Menu.toPourcentHeight(100.0, windowHeigth));
-            descriptionsBackground[i].setStyle("-fx-background-color: transparent;");
-            descriptionsBackground[i].setTranslateY(1);
-            StackPane.setAlignment(descriptionsBackground[i], Pos.BOTTOM_CENTER);
 
             // positionnement de la description
             descriptionText[i] = new Label();
@@ -78,23 +109,10 @@ public class SaveSelectionMenu extends MainMenu {
             //labels[i].setStyle("-fx-padding: 0 0 10 0;"); // Ajouter un padding pour déplacer le texte vers le bas
             buttonsContainer[i] = new StackPane();
             buttonsContainer[i].setAlignment(Pos.CENTER);
-            buttonsContainer[i].getChildren().addAll(buttons[i], buttonsText[i], descriptionsBackground[i], descriptionText[i]);
+            buttonsContainer[i].getChildren().addAll(buttons[i], buttonsText[i], descriptionText[i]);
+            buttonsContainer[i].setMaxHeight(buttons[i].getMaxHeight());
 
             //translation text animation
-            clipRectangle[i] = new Rectangle(buttons[i].getPrefWidth(), Menu.toPourcentHeight(100.0, windowHeigth));
-            descriptionsBackground[i].getStyleClass().add("description-background");
-            descriptionsBackground[i].setClip(clipRectangle[i]);
-            descriptionsBackground[i].setFocusTraversable(false);
-            descriptionsBackground[i].setMouseTransparent(true);
-
-            rectangleTransition[i] = new TranslateTransition(Duration.seconds(0.2), clipRectangle[i]);
-            rectangleTransition[i].setFromY(Menu.toPourcentHeight(200.0, windowHeigth));
-            rectangleTransition[i].setToY(0);
-
-            rectangleTransitionReverse[i] = new TranslateTransition(Duration.seconds(0.2), clipRectangle[i]);
-            rectangleTransitionReverse[i].setFromY(0);
-            rectangleTransitionReverse[i].setToY(Menu.toPourcentHeight(200.0, windowHeigth));
-
             fadeTransition[i] = new FadeTransition(Duration.seconds(0.3), descriptionText[i]);
             fadeTransition[i].setFromValue(0.0);
             fadeTransition[i].setToValue(1.0);
@@ -107,7 +125,14 @@ public class SaveSelectionMenu extends MainMenu {
             int finalI = i;
             buttonsContainer[i].setOnMouseEntered(e -> {
                 descriptionText[finalI].setTextAlignment(TextAlignment.CENTER);
-                descriptionText[finalI].setText(lstSave.get(finalI));
+                if (finalI >= lstSave.size()) descriptionText[finalI].setText("Rien");
+                else {
+                    String difficulte = extraireDifficulte(lstSave.get(finalI));
+                    String taille = extraireTaille(lstSave.get(finalI));
+                    String date = extraireDate(lstSave.get(finalI));
+                    String description = "Difficulté : " + difficulte + "\nTaille : " + taille + "\nDate : " + date;
+                    descriptionText[finalI].setText(description);
+                }
 
                 buttons[finalI].setOnAction(new EventHandler<ActionEvent>() {
                     @Override
@@ -115,51 +140,44 @@ public class SaveSelectionMenu extends MainMenu {
                         List<String> lstSave = CatalogueSauvegarde.listerSauvegarde(Launcher.catalogueProfils.getProfilActuel());
                         Main.showSaveSelectionMenu();
                         if (!lstSave.isEmpty()) {
-                            String saveName = lstSave.get(0);
+                            String saveName;
+                            if(finalI >= lstSave.size()) saveName = "Rien";
+                            else saveName = lstSave.get(finalI);
                             System.out.println("Chargement de la sauvegarde : " + saveName);
                             PartieSauvegarde save = PartieSauvegarde.chargerSauvegarde(saveName,Launcher.catalogueProfils.getProfilActuel());
                             Partie partie = Partie.chargerPartie(save,Launcher.catalogueProfils.getProfilActuel());
                         } else {
                             System.out.println("Aucune sauvegarde trouvée");
                         }
-
                     }
                 });
 
-                descriptionsBackground[finalI].setStyle("-fx-background-color: gray;");
-                rectangleTransition[finalI].play();
                 fadeTransition[finalI].play();
             });
 
             // hover off
             int finalI1 = i;
             buttonsContainer[i].setOnMouseExited(e -> {
-                rectangleTransition[finalI1].stop();
                 fadeTransition[finalI1].stop();
-                rectangleTransitionReverse[finalI1].play();
                 fadeTransitionReverse[finalI1].play();
             });
-
-            buttonsContainer[i].setMaxSize(buttonsContainer[i].getPrefWidth(), buttons[i].getPrefHeight());
         }
 
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(50);
-        gridPane.setVgap(10);
+        HBox box = new HBox();
+        box.setSpacing(50);
 
         for (int i = 0; i < buttonsContainer.length; i++) {
-            gridPane.add(buttonsContainer[i], i % 5, i / 5);
-            gridPane.setAlignment(Pos.CENTER);
+            box.getChildren().add(buttonsContainer[i]);
+            box.setAlignment(Pos.CENTER);
         }
 
-        ScrollPane scrollPane = new ScrollPane(gridPane);
+        ScrollPane scrollPane = new ScrollPane(box);
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
 
         StackPane stack = new StackPane();
         stack.getChildren().add(scrollPane);
         stack.setAlignment(Pos.CENTER);
-
         StackPane.setAlignment(scrollPane, Pos.CENTER);
 
         return stack;
