a26fcebaa37c71148e83d8c86b9fc406690d3e45
Sultan <38288876+yusuf72100@users.noreply.github.com>
2024-04-09 17:22:41 +0200

    historiqueAidesArea animations

--- src/main/java/groupe6/affichage/historiqueAidesArea.java
+++ src/main/java/groupe6/affichage/historiqueAidesArea.java
@@ -2,6 +2,7 @@ package groupe6.affichage;
 
 import groupe6.launcher.Launcher;
 import groupe6.model.partie.aide.AideInfos;
+import javafx.animation.*;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
@@ -10,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -17,6 +19,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,14 +60,17 @@ public class historiqueAidesArea {
      */
     private final Double height;
 
+    private final VBox elementsBox;
+
     public historiqueAidesArea(Double w, Double h) {
+        this.elementsBox = new VBox();
         this.width = w;
         this.height = h;
         this.aidesInfosList = new ArrayList<>();
         this.titre = new Label("Aides");
         this.titre.getStyleClass().add("title_help");
         this.historiqueAidesStackPane = new StackPane();
-        this.historiqueAidesVBox = new VBox(titre);
+        this.historiqueAidesVBox = new VBox();
         this.historiqueAidesVBox.setPrefSize(Menu.toPourcentWidth(800.0, 800.0) , h);
         this.historiqueAidesVBox.setAlignment(Pos.TOP_CENTER);
         this.historiqueAidesScrollPane = new ScrollPane(this.historiqueAidesVBox);
@@ -73,7 +79,9 @@ public class historiqueAidesArea {
         this.historiqueAidesStackPane.getChildren().add(this.historiqueAidesScrollPane);
         this.historiqueAidesStackPane.setAlignment(Pos.CENTER_RIGHT);
         this.historiqueAidesVBox.setSpacing(50);
+        this.elementsBox.setSpacing(50);
         this.historiqueAidesVBox.setStyle("-fx-background-color: #e0ac1e;");
+        this.historiqueAidesVBox.getChildren().addAll(this.titre, elementsBox);
         this.historiqueAidesVBox.setPadding(new Insets(0, 30, 0, 15));
         this.titre.setTranslateY(Menu.toPourcentHeight(200.0, 200.0));
 
@@ -120,7 +128,7 @@ public class historiqueAidesArea {
         String cheminLevel = Launcher.normaliserChemin(Launcher.dossierAssets + "/icon/level" + level + ".png");
         final ImageView[] imageLevel = {new ImageView(Launcher.chargerImage(cheminLevel))};
         Label niveau = new Label("Niveau");
-        Label descriptionText = new Label(description);
+        Label descriptionText = new Label();
         HBox header = new HBox(niveau, imageLevel[0], upgradeHelp, more);
         Rectangle separator = new Rectangle(this.historiqueAidesVBox.getPrefWidth()-30, 5);
         VBox aideNode = new VBox(header, descriptionText, separator);
@@ -139,10 +147,41 @@ public class historiqueAidesArea {
         Menu.adaptTextSize(descriptionText, 20, this.width, this.height);
         niveau.getStyleClass().add("aideNiveau");
 
-        this.historiqueAidesVBox.getChildren().add(1, aideNode);
         aideNode.setAlignment(Pos.CENTER_LEFT);
         aideNode.setMinSize(this.historiqueAidesVBox.getMaxWidth(), Region.USE_PREF_SIZE);  // fixer la taile d'un composant de la liste
         aideNode.setMaxSize(this.historiqueAidesVBox.getMaxWidth(), Region.USE_PREF_SIZE);  // fixer la taile d'un composant de la liste
+        this.elementsBox.getChildren().add(0, aideNode);
+
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, e -> {
+            descriptionText.setText("");
+        }));
+
+        for (int i = 0; i < description.length(); i++) {
+            final int index = i;
+            KeyFrame keyFrame = new KeyFrame(Duration.millis((i + 1) * 30), e -> {
+                descriptionText.setText(descriptionText.getText() + description.charAt(index));
+            });
+            timeline.getKeyFrames().add(keyFrame);
+        }
+
+        FadeTransition fade = new FadeTransition(Duration.millis(300), descriptionText);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+        timeline.play();
+
+        FadeTransition ft = new FadeTransition(Duration.millis(300), aideNode);
+        ft.setFromValue(0.0);
+        ft.setToValue(1.0);
+        ft.play();
+
+        ScaleTransition scale = new ScaleTransition(Duration.millis(300), aideNode);
+        scale.setFromX(0);
+        scale.setFromY(0);
+        scale.setToX(1);
+        scale.setToY(1);
+        scale.play();
 
         int index = aidesInfosList.size();
 
@@ -172,6 +211,7 @@ public class historiqueAidesArea {
             @Override
             public void handle(ActionEvent event) {
                 // TODO : Popup avec les schémas explicatifs ...
+                ajouterNouvelleAide(3, "Nouvelle Aide2abcdefghijklmnopqrstuvwxyzAide2abcdefghijklmnopqrstuvwxyz");
             }
         });
     }
